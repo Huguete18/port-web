82b58050934885e776cc26536fceea911ff845c7
Huguete18 <serhuguet@protonmail.com>
2023-11-21 20:33:41 +0100

    Se agrega archivo

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Image from 'next/image';
 
 const Footer = () => {
   return (
     <footer className='footer border z-10 border-t-[#33353f] border-l-transparent border-r-transparent text-white'>
         <div className='container p-12 flex justify-between'>
-            <span>LOGO</span>
+            <span>
+              <Image src='/image/2560px-SHV.png' style={{ borderRadius: '10px' }} alt='' width={100} height={100} />
+            </span>
             <p className='text-slate-600'>All right reserved.</p>
         </div>
     </footer>
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,11 +1,12 @@
 "use client";
 import React, { useState } from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 import NavLink from './NavLink';
+import MenuOverlay from './MenuOverlay';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { faX } from '@fortawesome/free-solid-svg-icons';
-import MenuOverlay from './MenuOverlay';
 
 const navLinks = [
     {
@@ -27,7 +28,9 @@ const Navbar = () => {
     return (
         <nav className='fixed mx-auto border border-[#33353f] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100'>
             <div className='flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2'>
-                <Link href={'/'} className='text-2xl md:text-2xl text-white font-semibold'>LOGO</Link>
+                <Link href={'/'} className='text-2xl md:text-2xl text-white font-semibold'>
+                    <Image src='/image/SHV.png' style={{ borderRadius: '10px' }} alt='' width={100} height={100} />
+                </Link>
                 <div className='mobile-menu block md:hidden'>
                     {!navbarOpen ? (
                         <button onClick={() => setNavbarOpen(true)} className='flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white'>
