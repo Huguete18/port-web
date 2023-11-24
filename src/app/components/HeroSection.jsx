56e678e37bafae0158e5bad02873aac2550d8661
Huguete18 <serhuguet@protonmail.com>
2023-11-24 21:39:20 +0100

    Se agrega archivo

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -3,46 +3,52 @@ import React from 'react';
 import Image from 'next/image';
 import { TypeAnimation } from 'react-type-animation';
 import { motion } from 'framer-motion';
+import { saveAs } from 'file-saver';
 
 const HeroSection = () => {
-  return (
-    <section className='lg:py-16'>
-        <div className='grid grid-cols-1 sm:grid-cols-12'>
-            <motion.div initial={{ opacity: 0, scale:0.5 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 0.5 }} className='col-span-7 place-self-center text-center sm:text-left justify-self-start'>
-                <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold'>
-                    <span className='text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600'>Hello, {'I\'m'}</span>
-                    <br />
-                    <TypeAnimation
-                        sequence={[
-                        'Sergi Huguet',
-                        1000,
-                        'Web Developer',
-                        1000,
-                        'UI/UX Designer',
-                        1000,
-                        'Cybersecurity',
-                        1000
-                        ]} 
-                        wrapper="span" speed={50} repeat={Infinity}/>
-                </h1>
-                <p className='text-[#adb7be] text-base sm:text-lg mb-6 lg:text-xl'>
-                    {'I\'m'} a passionate web develop and Cybersecurity.
-                </p>
-                <div>
-                    <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-primary-500 to-secondary-500 hover:bg-slate-200 text-white'>Hire Me</button>
-                    <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3'>
-                        <span className='block bg-[#1f2430] hover:bg-slate-800 rounded-full px-5 py-2'>Download CV</span>
-                    </button>
-                </div>
-            </motion.div>
-            <motion.div initial={{ opacity: 0, scale:0.5 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 0.5 }} className='col-span-4 place-self-center mt-4 lg:mt-0'>
-                <div className='rounded-full bg-[#151920] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative'>
-                    <Image src='/image/hero-image.png' alt='hero image' className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2' width={300} height={300}/>
-                </div>
-            </motion.div>
-        </div>
-    </section>
-  );
+    const handleDownload = () => {
+        const pdfUrl = '/pdf/CV_Huguet_Sergi.pdf';
+        const fileName = pdfUrl.substring(pdfUrl.lastIndexOf('/') + 1);
+        saveAs(pdfUrl, fileName);
+      };
+    return (
+        <section className='lg:py-16'>
+            <div className='grid grid-cols-1 sm:grid-cols-12'>
+                <motion.div initial={{ opacity: 0, scale:0.5 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 0.5 }} className='col-span-7 place-self-center text-center sm:text-left justify-self-start'>
+                    <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold'>
+                        <span className='text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600'>Hello, {'I\'m'}</span>
+                        <br />
+                        <TypeAnimation
+                            sequence={[
+                            'Sergi Huguet',
+                            1000,
+                            'Web Developer',
+                            1000,
+                            'UI/UX Designer',
+                            1000,
+                            'Cybersecurity',
+                            1000
+                            ]} 
+                            wrapper="span" speed={50} repeat={Infinity}/>
+                    </h1>
+                    <p className='text-[#adb7be] text-base sm:text-lg mb-6 lg:text-xl'>
+                        {'I\'m'} a passionate web develop and Cybersecurity.
+                    </p>
+                    <div>
+                        <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-primary-500 to-secondary-500 hover:bg-slate-200 text-white'>Hire Me</button>
+                        <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3'>
+                            <span className='block bg-[#1f2430] hover:bg-slate-800 rounded-full px-5 py-2' onClick={handleDownload}>Download CV</span>
+                        </button>
+                    </div>
+                </motion.div>
+                <motion.div initial={{ opacity: 0, scale:0.5 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 0.5 }} className='col-span-4 place-self-center mt-4 lg:mt-0'>
+                    <div className='rounded-full bg-[#151920] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative'>
+                        <Image src='/image/hero-image.png' alt='hero image' className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2' width={300} height={300}/>
+                    </div>
+                </motion.div>
+            </div>
+        </section>
+    );
 };
 
 export default HeroSection;
